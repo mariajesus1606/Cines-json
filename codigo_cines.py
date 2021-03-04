6c05d1bbf37486028b15e2a1a353034de4f22791
Maria Jesus <bpmariajesus20@gmail.com>
2021-03-04 10:13:34 +0100

    He modificado el fichero codigo_cines.py

--- codigo_cines.py
+++ codigo_cines.py
@@ -1,5 +1,21 @@
 import json
+with open ('cines.json','r') as fichero:
+    doc=json.load(fichero)
 
 #Listar información: Listar las películas que tiene cada cine y sus sesiones.
-def listar_pelisysesiones():
-  
+def listar_pelisysesiones(doc):
+    cines = []
+    for cine in doc["cines"]:
+        cines.append(cine["cines"])
+    return cines 
+
+#Contar información: Lista los cines y cuéntame cuantas películas hay en cada una de ellos.
+
+
+#Buscar o filtrar información: Pide por teclado una película y te dice el director de esa película.
+
+
+#Buscar información relacionada: Pedir por teclado una sesión, y que te muestre los cines, las películas y la hora a la que es la película.
+
+
+#Ejercicio libre: Pide por teclado una película y te muestra la sinopsis
